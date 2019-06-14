2d9fd178a212f7d934c0fa6226e54e8c934fda79
mtp1032 <mtpeterson1948@gmail.com>
2019-06-14 17:05:50 -0600

    Interim update
    Basic event response flow is working. It's but a skeleton of what it will be at some point

--- Libs/ContainerEventHandler.lua
+++ Libs/ContainerEventHandler.lua
@@ -49,31 +49,12 @@ end
 --********************************************************************************
 --						EVENT HANDLERS
 --********************************************************************************
-local function eventBagOpened( bagId )
+local function eventBagUpdate( bagSlot )
 	local result = STATUS_SUCCESS
-	local msg = string.format("Bag[%d] opened!\n", bagId )
-	-- mf:postMsg( msg )
+	local msg = string.format("Bag[%d] updated!\n", bagSlot )
+	mf:postMsg( msg )
 	return result
 end
-local function eventBagClosed( bagId )
-	local result = STATUS_SUCCESS
-	local msg = string.format("Bag[%d] closed!\n", bagId )
-	-- mf:postMsg( msg )
-	return result
-end
-local function eventBagUpdated( bagId )
-	local result = STATUS_SUCCESS
-	local msg = string.format("Bag[%d] updated!\n", bagId )
-	-- mf:postMsg( msg )
-	return result
-end
-local function eventBagUpdateCooldown( bagId )
-	local result = STATUS_SUCCESS
-	local msg = string.format("Bag[%d] updated!\n", bagId )
-	-- mf:postMsg( msg )
-	return result
-end
-
 
 --********************************************************************************
 --						CREATES THE EVENT HANDLING FRAME AND CALLS
@@ -84,34 +65,31 @@ local eventFrame = CreateFrame("Frame" )
 	eventFrame:RegisterEvent("BAG_OPEN") 
 	eventFrame:RegisterEvent("BAG_CLOSED")
 	eventFrame:RegisterEvent("BAG_UPDATE")
+	eventFrame:RegisterEvent("ITEM_LOCKED") -- arg1 bagSlot, arg2 slotId of item
+
+	-- if arg2 ~= nil, arg1 is bagSlot, arg2 is slotId
+	-- if arg2 is nil, arg1 is equipment slot of item
+-- 	Usually fires in pairs when an item is swapping with another.
+-- Empty slots do not lock.
+-- GetContainerItemInfo and IsInventoryItemLocked can be used to query lock status.
+-- This does NOT fire on ammo pickups.
+	eventFrame:RegisterEvent("ITEM_LOCK_CHANGED")
+	eventFrame:RegisterEvent("ITEM_UNLOCKED")
+	eventFrame:RegisterEvent("ITEM_PUSH") 
+	eventFrame:RegisterEvent("UNIT_INVENTORY_CHANGED")
+	eventFrame:RegisterEvent("BAG_UPDATE")
+	
 	eventFrame:SetScript("OnEvent", 
 	function( self, event, ... )
-		local bagId = ...
-		local result = SUCCESSFUL_RESULT
+		local bagSlot, slotId = ...
 
-		-- BAG_OPEN IS Fired when a bag (NOTE: This is NOT fired for player containers, 
-		-- it's for those bag-like objects that you can remove items from 
-		-- but not put items into) is opened.
-if event == "BAG_OPEN" then
-			result = eventBagOpened( bagId )
-			-- if result[1] ~= STATUS_SUCCESS then
-			-- 	E:postResult( result )
-			-- end
-			return
-		end
-
-		-- BAG_CLOSED is fired when a bag is [re]moved from its bagslot. Fires
-		-- for both player and bank bags
-		if event == "BAG_CLOSED" then
-			result = eventBagClosed( bagId )
-			-- if result[1] ~= STATUS_SUCCESS then
-			-- 	E:postResult( result )
-			-- end
-			return
-		end
+		-- if event == "BAG_UPDATE" then
+		-- 	local b = Bag( bagSlot )
+		-- 	b:bagUpdate( bagSlot )
+		-- end
 
 		-- BAG_UPDATE is fired when a bags inventory changes, including items
-		-- being moved from slot to another.  Bag zero, the 
+		-- being moved from one slot to another.  Bag zero, the 
 		-- sixteen slot default backpack, may not fire on login. Upon login 
 		-- (or reloading the console) this event fires even for bank bags. 
 		-- When moving an item in your inventory, this fires multiple 
@@ -119,22 +97,16 @@ if event == "BAG_OPEN" then
 		-- involved is the default backpack, this event will also fire with 
 		-- a container ID of "-2" (twice if you are moving the item inside 
 		-- the same bag)
-		if event == "BAG_UPDATE" then
-			result = eventBagUpdated( bagId )
-			-- if result[1] ~= STATUS_SUCCESS then
-			-- 	E:postResult( result )
-			-- end
-			return
+		-- if event == "BAG_UPDATE" then
+		-- 	mf:postMsg( msg )
+		-- 	result = eventBagUpdate( bagSlot )
+		-- 	if result[1] ~= STATUS_SUCCESS then
+		-- 		E:postResult( result )
+		-- 	end
+		-- 	return
+		-- end
+		if event == "ITEM_LOCK_CHANGED" then
+			local b = Bag( bagSlot )
+			b:itemLockChanged( bagSlot, slotId )
 		end
-
-		-- BAG_UPDATE_COOLDOWN is fired when a cooldown update call is 
-		-- sent to a bag. The container Id may be nil
-		if event == "BAG_UPDATE_COOLDOWN" then
-			result = eventBagUpdateCooldown( bagId )
-			-- if result[1] ~= STATUS_SUCCESS then
-			-- 	E:postResult( result )
-			-- end
-			return
-		end
-		mf:postMsg(event)
 	end)
